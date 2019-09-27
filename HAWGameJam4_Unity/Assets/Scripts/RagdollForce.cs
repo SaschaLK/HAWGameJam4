ef768e2ebf1de9d1c3340b5a7711553d7fb54ab2
Nenad Slavujevic <nenad.slavujevic@posteo.de>
2019-09-28 00:05:17 +0200

    basic rag doll

---
+++ HAWGameJam4_Unity/Assets/Scripts/RagdollForce.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollForce : MonoBehaviour
+{
+    private int counter;
+
+    private float randomValueOne = -0.00005f;
+    private float randomValueTwo = 0.00005f;
+    
+    void Update()
+    {
+        counter++;
+
+        if (counter == 10)
+        {
+            GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(randomValueOne,randomValueTwo),Random.Range(randomValueOne,randomValueTwo), 0));
+            
+            counter = 0;
+        }
+    }
+}
---
+++ HAWGameJam4_Unity/Assets/Scripts/RagdollRotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollRotate : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(Vector3.right * (80 * Time.deltaTime));
+        transform.Rotate(Vector3.forward * (80 * Time.deltaTime));
+    }
+}
