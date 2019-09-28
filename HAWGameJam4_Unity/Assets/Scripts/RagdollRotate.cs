53b97b479fa2b77910884188dc454b1e36ea2b77
Nenad Slavujevic <nenad.slavujevic@posteo.de>
2019-09-28 20:01:19 +0200

    jiggle

--- HAWGameJam4_Unity/Assets/Scripts/RagdollRotate.cs
+++ HAWGameJam4_Unity/Assets/Scripts/RagdollRotate.cs
@@ -6,7 +6,7 @@ public class RagdollRotate : MonoBehaviour
 {
     void Update()
     {
-        transform.Rotate(Vector3.right * (80 * Time.deltaTime));
-        transform.Rotate(Vector3.forward * (80 * Time.deltaTime));
+        //transform.Rotate(Vector3.right * (80 * Time.deltaTime));
+        //transform.Rotate(Vector3.forward * (80 * Time.deltaTime));
     }
 }
