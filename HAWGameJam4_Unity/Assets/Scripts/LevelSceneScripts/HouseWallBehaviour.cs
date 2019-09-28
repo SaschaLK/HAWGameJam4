0fa5679b4ad8478431978b533254e45416c89c9d
SaschaLK <SaschaL.Kufahl@gmail.com>
2019-09-28 13:21:50 +0200

    Scrolling walls

---
+++ HAWGameJam4_Unity/Assets/Scripts/LevelSceneScripts/HouseWallBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseWallBehaviour : MonoBehaviour {
+    private float scrollingSpeed;
+    private float ceiling;
+    private float floor;
+    public int id;
+    public int idLeader;
+
+    private void Start() {
+        scrollingSpeed = ScrollingManagerBehaviour.instance.wallSpeed;
+        ceiling = ScrollingManagerBehaviour.instance.ceiling;
+    }
+
+    private void Update() {
+        if(gameObject.transform.position.y >= ceiling) {
+            ScrollingManagerBehaviour.instance.ResetWallTile(id, idLeader);
+        }
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + scrollingSpeed*Time.deltaTime, gameObject.transform.position.z);
+    }
+}
