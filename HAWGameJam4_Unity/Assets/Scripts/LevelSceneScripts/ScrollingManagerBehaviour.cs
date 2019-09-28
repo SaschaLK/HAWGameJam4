fb42f2fcc26047dbfd9a3319a6b6354645baa5b5
Nenad Slavujevic <nenad.slavujevic@posteo.de>
2019-09-28 16:34:15 +0200

    basic level

--- HAWGameJam4_Unity/Assets/Scripts/LevelSceneScripts/ScrollingManagerBehaviour.cs
+++ HAWGameJam4_Unity/Assets/Scripts/LevelSceneScripts/ScrollingManagerBehaviour.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScrollingManagerBehaviour : MonoBehaviour {
+public class ScrollingManagerBehaviour : MonoBehaviour 
+{
     public static ScrollingManagerBehaviour instance;
-
+    
     public float wallSpeed;
-    public int wallCount;
-    public List<GameObject> houseWallsPrefabs;
-    private List<GameObject> houseWalls;
-
     public float ceiling;
-    public float floor;
-
+    
+    [SerializeField] float floor;
+    [SerializeField] int wallCount;
+    [SerializeField] float scaleVector;
+    [SerializeField] List<GameObject> houseWallsPrefabs;
+    
+    private List<GameObject> houseWalls;
+    
     private void Awake() {
         instance = this;
     }
@@ -21,22 +24,34 @@ public class ScrollingManagerBehaviour : MonoBehaviour {
         houseWalls = new List<GameObject>();
 
         int idCount = 0;
-        for(int i = 0; i < wallCount; i++) {
-            houseWalls.Add(Instantiate(houseWallsPrefabs[Random.Range(0, houseWallsPrefabs.Count - 1)], new Vector3(0, i, 0), Quaternion.identity));
+        
+        for(int i = 0; i < wallCount; i++) 
+        {
+            houseWalls.Add(Instantiate(houseWallsPrefabs[Random.Range(0, houseWallsPrefabs.Count - 1)],
+                new Vector3(-1*scaleVector, i * scaleVector, 0), Quaternion.identity));
             houseWalls[idCount].GetComponent<HouseWallBehaviour>().id = idCount;
-            if (wallCount * 2 == idCount + 2) {
+            
+            if (wallCount * 2 == idCount + 2) 
+            {
                 houseWalls[idCount].GetComponent<HouseWallBehaviour>().idLeader = 0;
             }
-            else {
+            else 
+            {
                 houseWalls[idCount].GetComponent<HouseWallBehaviour>().idLeader = idCount + 2;
             }
+            
             idCount++;
-            houseWalls.Add(Instantiate(houseWallsPrefabs[Random.Range(0, houseWallsPrefabs.Count - 1)], new Vector3(2, i, 0), Quaternion.identity));
+            
+            houseWalls.Add(Instantiate(houseWallsPrefabs[Random.Range(0, houseWallsPrefabs.Count - 1)],
+                new Vector3(1*scaleVector, i * scaleVector, 0), Quaternion.identity));
             houseWalls[idCount].GetComponent<HouseWallBehaviour>().id = idCount;
-            if(wallCount*2 == idCount + 1) {
+            
+            if(wallCount*2 == idCount + 1) 
+            {
                 houseWalls[idCount].GetComponent<HouseWallBehaviour>().idLeader = 1;
             }
-            else {
+            else 
+            {
                 houseWalls[idCount].GetComponent<HouseWallBehaviour>().idLeader = idCount + 2;
             }
             idCount++;
@@ -50,6 +65,6 @@ public class ScrollingManagerBehaviour : MonoBehaviour {
     }
 
     public void ResetWallTile(int id, int idLeader) {
-        houseWalls[id].transform.position = new Vector3(houseWalls[id].transform.position.x, houseWalls[idLeader].transform.position.y - 1, houseWalls[id].transform.position.z);
+        houseWalls[id].transform.position = new Vector3(houseWalls[id].transform.position.x, houseWalls[idLeader].transform.position.y - scaleVector, houseWalls[id].transform.position.z);
     }
 }
