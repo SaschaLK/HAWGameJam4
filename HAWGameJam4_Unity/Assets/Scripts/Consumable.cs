c9a625a381f7bdb45a7d1e24951ace4fff6f01d2
Nenad Slavujevic <nenad.slavujevic@posteo.de>
2019-09-29 10:50:55 +0200

    consumables points

---
+++ HAWGameJam4_Unity/Assets/Scripts/Consumable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consumable : MonoBehaviour
+{
+    [SerializeField] private int value;
+    
+    void OnCollisionEnter(Collision other)
+    {
+        // Get player number
+        int id = 1;
+        
+        GameManager.instance.UpdatePlayerScore(id, value);
+    }
+}
